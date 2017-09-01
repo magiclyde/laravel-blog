2c90928e3bfa811d00b8cf7e9d3438e767d78dba
magiclyde <clyde66@163.com>
2017-09-01 03:27:11 -0700

    render gallery page

--- resources/views/admin/gallery.blade.php
+++ resources/views/admin/gallery.blade.php
@@ -5,6 +5,92 @@
 @endsection
 
 @section('content')
+<div class="row">
+    <div class="col-lg-12">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>Gallery carousel</h5>
+                <div class="ibox-tools">
+                    <a class="collapse-link">
+                        <i class="fa fa-chevron-up"></i>
+                    </a>
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-wrench"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#">Config option 1</a>
+                        </li>
+                        <li><a href="#">Config option 2</a>
+                        </li>
+                    </ul>
+                    <a class="close-link">
+                        <i class="fa fa-times"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="ibox-content">
+                <div class="carousel slide" id="carousel3">
+                    <div class="carousel-inner">
+                        <div class="item gallery active left">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <img alt="image" class="img-responsive" src="{{ asset('inspiration/img/p_big1.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image" class="img-responsive" src="{{ asset('inspiration/img/p_big2.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive"  src="{{ asset('inspiration/img/p_big3.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive" src="{{ asset('inspiration/img/p_big1.jpg') }}">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item gallery next left">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive" src="{{ asset('inspiration/img/p_big3.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive" src="{{ asset('inspiration/img/p_big1.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive"  src="{{ asset('inspiration/img/p_big2.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive" src="{{ asset('inspiration/img/p_big1.jpg') }}">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item gallery">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive" src="{{ asset('inspiration/img/p_big2.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive" src="{{ asset('inspiration/img/p_big3.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image"  class="img-responsive"  src="{{ asset('inspiration/img/p_big1.jpg') }}">
+                                </div>
+                                <div class="col-sm-6">
+                                    <img alt="image" class="img-responsive" src="{{ asset('inspiration/img/p_big2.jpg') }}">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a data-slide="prev" href="#carousel3" class="left carousel-control">
+                        <span class="icon-prev"></span>
+                    </a>
+                    <a data-slide="next" href="#carousel3" class="right carousel-control">
+                        <span class="icon-next"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Custom and plugin javascript -->
 <script src="{{ asset('inspiration/js/inspinia.js') }}"></script>
